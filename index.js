c99822bd2466caa815a4a66c8738918f4912936b
EBPC-01190 <ebpc-01190@EBPC-01190s-MacBook-Pro.local>
2019-02-28 22:37:46 -0600

    GET requests complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+var http = require('http');
+let state = require("./state");
 
+let server = http.createServer(messageReceived);
+
+server.listen(8080);
+
+function messageReceived(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    if(req.method === "GET" && req.url === "/users"){
+        res.write(JSON.stringify(state.users));
+    }
+    else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
+        let id = req.url.split("/");
+        let user = state.users.find(p=>p["_id"] == id[2]);
+        let usersJSON = JSON.stringify(user);
+        res.write(usersJSON);
+    }
+    else if(req.method === "POST" && req.url === "/products"){
+        postProducts(req, res);
+    }
+    else{
+        res.write("So Sorry. No Data For You!");
+    }
+    res.end();
+}   
+
+function postProducts(req, res){
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      let product = JSON.parse(body);
+      products.push(product);
+    }); 
+}
