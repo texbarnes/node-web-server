96bd618b1e6b22a1ee4f9952e2b4035423ce2ded
EBPC-01190 <ebpc-01190@EBPC-01190s-MacBook-Pro.local>
2019-03-01 00:54:39 -0600

    most of the way to the goal.

--- README.md
+++ README.md
@@ -6,20 +6,21 @@ A starter repo for the ACA Advanced `My First Web Server` project.
 ## Part 1.
 * [X] Give your server the ability to respond to a GET request with a path "/users" and return the users array from state.js
 * [X] Give your server the ability to respond to a GET request with a path "/users/1" and return the first user object from the users array from state.js
-* Give your server the ability to respond to a POST request with a path "/users" and just add a hard coded user object to the users array from state.js. .json() the last user in the array to send it back to the client. (if you do another GET request you should see this added)
-* Give your server the ability to respond to a PUT request with a path "/users/1" and just change any key value on the first user object in the users array in state.js. .json() this user to send it back to the client.
-* Give your server the ability to respond to a DELETE request with a path "/users/1" and remove one item from the users array. send() back a messsage "deleted"
+* [X] Give your server the ability to respond to a POST request with a path "/users" and just add a hard coded user object to the users array from state.js. .json() the last user in the array to send it back to the client. (if you do another GET request you should see this added)
+* [X] Give your server the ability to respond to a PUT request with a path "/users/1" and just change any key value on the first user object in the users array in state.js. .json() this user to send it back to the client.
+* [X] Give your server the ability to respond to a DELETE request with a path "/users/1" and remove one item from the users array. send() back a messsage "deleted"
 
 
 ## Part 2. Body
-* Give your server the ability to handle a POST request with a path "/users" and add the body from the client to the users array
-* Assign an _id property to the user object that is a number that increments by 1 each time.
+* [X] Give your server the ability to handle a POST request with a path "/users" and add the body from the client to the users array
+* [X] Assign an _id property to the user object that is a number that increments by 1 each time.
 * Send the newly created user object back to the client
 
 ## Part 3. Use path variables
 * [X] Give your server the ability to respond to a GET request with a path `/users/:userId` and return the user object from the users array that has the _id == userId
-* Give your server the ability to respond to a PUT request with a path `/users/:userId` and just change any key value on the user object with this _id 
-* Give your server the ability to respond to a DELETE request with a path `/users/:userId` and find the user with this id from the array. give this user object a new key value isActive:false.  send() back a messsage "deleted"
+* [X] Give your server the ability to respond to a PUT request with a path `/users/:userId` and just change any key value on the user object with this _id 
+* [X] Give your server the ability to respond to a DELETE request with a path `/users/:userId` and find the user with this id from the array. give this user object a new key value isActive:false.  send() back a messsage "deleted"
  
  
  ## Part 4. Do the same thing for products
+ [X]
--- index.js
+++ index.js
@@ -6,15 +6,69 @@ let server = http.createServer(messageReceived);
 server.listen(8080);
 
 function messageReceived(req, res) {
+    // I feel like there should be a way to split the path here, and
+    // depending upon the path, use the same code below 1X, rather than
+    // creating duplicate code per object (Users, Products) ¯\_(ツ)_/¯
+    
+    // I attempted to code this ^^^ but ran into difficulty... h m m m
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
     if(req.method === "GET" && req.url === "/users"){
         res.write(JSON.stringify(state.users));
+    }
+    else if(req.method === "GET" && req.url === "/products"){
+        res.write(JSON.stringify(state.products));
     }
     else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
         let id = req.url.split("/");
         let user = state.users.find(p=>p["_id"] == id[2]);
         let usersJSON = JSON.stringify(user);
         res.write(usersJSON);
+    }
+    else if(req.method === "GET" && req.url.indexOf("/products/") > -1){
+        let id = req.url.split("/");
+        let product = state.products.find(p=>p["id"] == id[2]);
+        let productsJSON = JSON.stringify(product);
+        res.write(productsJSON);
+    }
+    else if(req.method === "PUT" && req.url.indexOf("/users/") > -1){
+        let id = req.url.split("/");
+        let user = state.users.find(p=>p["_id"] == id[2]);
+        let body = [];
+        req.on('data', (chunk) => {
+        body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            body = JSON.parse(body);
+            user.name = body.name;
+            // not sure how to update any given "key": "value" pair?
+        });
+    }
+    else if(req.method === "PUT" && req.url.indexOf("/products/") > -1){
+        let id = req.url.split("/");
+        let product = state.products.find(p=>p["id"] == id[2]);
+        let body = [];
+        req.on('data', (chunk) => {
+        body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            body = JSON.parse(body);
+            product.name = body.name;
+            // not sure how to update any given "key": "value" pair?
+        });
+    }
+    else if(req.method === "DELETE" && req.url.indexOf("/users/") > -1){
+        let id = req.url.split("/");
+        state.users.splice(Number(id[2])-1, 1);
+        res.write("deleted");
+    }
+    else if(req.method === "DELETE" && req.url.indexOf("/products/") > -1){
+        let id = req.url.split("/");
+        state.products.splice(Number(id[2])-1, 1);
+        res.write("deleted");
+    }
+    else if(req.method === "POST" && req.url === "/users"){
+        postUsers(req, res);
     }
     else if(req.method === "POST" && req.url === "/products"){
         postProducts(req, res);
@@ -25,6 +79,19 @@ function messageReceived(req, res) {
     res.end();
 }   
 
+function postUsers(req, res){
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      let user = JSON.parse(body);
+      user._id = state.users.length+1;
+      state.users.push(user);
+      // Need to send newly created user object back to client ¯\_(ツ)_/¯
+    }); 
+}
+
 function postProducts(req, res){
     let body = [];
     req.on('data', (chunk) => {
@@ -32,6 +99,7 @@ function postProducts(req, res){
     }).on('end', () => {
       body = Buffer.concat(body).toString();
       let product = JSON.parse(body);
-      products.push(product);
+      product.id = state.products.length+1;
+      state.products.push(product);
     }); 
 }
